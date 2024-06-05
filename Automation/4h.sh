b8cec9b64c6c2fc31f7a4921c4ad3c244b9bcd1d
L1ZLe <samielyaagoubius@gmail.com>
2024-06-05 18:43:01 +0100

    saving 4h

--- Automation/4h.sh
+++ Automation/4h.sh
@@ -4,12 +4,12 @@
 cd /home/l1zle/Investment-manager/4H
 
 # Run your Node.js script and store its output in last_output.txt
-/home/l1zle/.nvm/versions/node/v20.12.1/bin/node index.js &> /home/l1zle/Investment-manager/Automation/last_output_4h.txt && wait
+node index.js &> /home/l1zle/Investment-manager/Automation/last_output_4h.txt && wait
 
 cd /home/l1zle/Investment-manager/Automation
 
 # Run your last_run.js script (output not stored)
-/home/l1zle/.nvm/versions/node/v20.12.1/bin/node last_run.js && wait
+node last_run.js && wait
 
 # Now, git commands will execute after both Node.js scripts have finished
 cd /home/l1zle/Investment-manager/
