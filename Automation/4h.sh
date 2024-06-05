1c6437af05b50a2fa2d62658528769711890449f
L1ZLe <samielyaagoubius@gmail.com>
2024-06-05 19:53:17 +0100

    saving 4h

--- Automation/4h.sh
+++ Automation/4h.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Change directory to where your Node.js script is located
-cd ~/investments-manager/4H
+cd ~/investments_manager/4H
 
 # Run your Node.js script and store its output in last_output.tx
-node index.js &> /data/data/com.termux/files/home/investment-manager/Automation/last_output_4h.txt && wait
+node index.js &> ~/investments_manager/Automation/last_output_4h.txt && wait
 
-cd /data/data/com.termux/files/home/investment-manager/Automation
+cd ~/investments_manager/Automation
 
 # Run your last_run.js script (output not stored)
 node last_run.js && wait
 
 # Now, git commands will execute after both Node.js scripts have finished
-cd  /data/data/com.termux/files/home/investment-manager/
+cd ~/investments_manager/
 git add .
 git commit -m "saving 4h"
 git push origin main
